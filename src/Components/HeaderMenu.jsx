c47196d0ed9ed5a8648bd3d95e63e195bcdd7890
johnycodestone <mwaqarsaleem1@gmail.com>
2025-08-06 15:14:11 +0500

    Created Basic Files for React Website - OmniFood

---
+++ src/Components/HeaderMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderMenuList from "./HeaderMenuList";
+
+const HeaderMenu = () => {
+    return (
+        <div className="header-menu">
+            
+        </div>
+    )
+}
+
+export default HeaderMenu;
