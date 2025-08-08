8b4dd2377e761b107a23e90a25b7bbf5059ea9f1
johnycodestone <mwaqarsaleem1@gmail.com>
2025-08-08 15:27:00 +0500

    Hero Section completed with mobile responsiveness.

--- src/Components/HeaderMenu.jsx
+++ src/Components/HeaderMenu.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import HeaderMenuList from "./HeaderMenuList";
 import Logo from "../Assets/Images/logo-white.png";
 import "../Styles/HeaderMenu.css"; // .. to go one folder back
+import {useState} from "react";
 
 const HeaderMenu = () => {
-    return (
-        <div className="header-menu">
-            <img src={Logo} alt="OmniFood Logo" className="header-logo" />
-            <nav className="header-nav">
-                <HeaderMenuList />
-            </nav>
-        </div>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  const menuClass = isOpen ? "open" : "";
+  const listClass = isOpen ? "" : "closed";
+  return (
+    <div className="header-menu">
+      <img src={Logo} alt="OmniFood Logo" className="header-logo" />
+      <nav className={`header-nav ${listClass}`}>
+        <HeaderMenuList />
+      </nav>
+      <button className={`hamburger-menu ${menuClass}`} onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+    </div>
+  );
+};
 
 export default HeaderMenu;
